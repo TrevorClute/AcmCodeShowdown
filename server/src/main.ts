048efeef64da7919ffe112bf615323af6f19a507
TrevorClute <121002446+TrevorClute@users.noreply.github.com>
2025-03-06 19:33:29 -0500

    b

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,13 +3,7 @@ import { AppModule } from './app.module';
 import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./src/cert/key.pem'),
-    cert: fs.readFileSync('./src/cert/cert.pem'),
-  };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
